45d15419ed1cee1cdd7c56f8d91a56ac9b9c5c17
sujishanthi <30317798+sujishanthi@users.noreply.github.com>
2017-07-28 11:52:53 +0530

    Create arrays.c

---
+++ arrays.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+     int marks[10], i, n, sum = 0, average;
+     printf("Enter n: ");
+     scanf("%d", &n);
+     for(i=0; i<n; ++i)
+     {
+          printf("Enter number%d: ",i+1);
+          scanf("%d", &marks[i]);
+          sum += marks[i];
+     }
+     average = sum/n;
+     printf("Average = %d", average);
+}
